092adb775f70ba1b452f5b5b921cb13819b5159d
Alt <anb@tsast.ru>
2016-12-22 17:19:15 +0300

    #2 Some fix

--- server.js
+++ server.js
@@ -11,20 +11,6 @@ app.get('/', function (req, res) {
   res.sendfile('./app/index.html')
 });
 
-app.get('/song', function (req, res) {
-  const song = './public/Digital Summer - Breaking Point.mp3';
-  fs.stat(song, function (err, stats) {
-    if(err) {
-      console.error(err);
-      return res.writeHead(500);
-    }
-
-    const readStream = fs.createReadStream(song);
-    res.writeHead(200, { 'Content-Type': 'audio/mpeg', 'Content-Length': stats.size });
-    res.pump(readStream);
-  })
-});
-
 app.listen(config.server.port, function () {
   console.log(`Alt.music app listening on port ${config.server.port}!`);
 });
