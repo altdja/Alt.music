68ea0a2e08bbd91475f54cda3e85385c451948f7
altdja <ramsone@yandex.ru>
2017-01-29 23:58:49 +0300

    Some changes

--- client/app/modules/home/controller.js
+++ client/app/modules/home/controller.js
@@ -5,11 +5,30 @@ import './styles.css';
 // import axios from 'axios';
 import $ from 'jquery';
 
+const src = 'http://91.240.87.220:8000/stream.mp3';
+
 export default class HomepageController {
   constructor($scope, $state) {
     this.scope = $scope;
     this.state = $state;
-    // this.moment = moment;
-    // this.audio = new window.AudioContext();
+    this.played = false;
+    this.output =  new Audio();
+    document.body.appendChild(this.output);
+  }
+  
+  play() {
+    if (!this.played) {
+      this.output.src = src;
+      this.output.play()
+        .then(() => {
+          console.log('!!!');
+        });
+      this.played = true;
+    } else {
+      $.each($('audio'), function () {
+        this.pause();
+      });
+      this.played = false;      
+    }
   }
 }
